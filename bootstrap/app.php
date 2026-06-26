46e36cf72e3d45c91326dfb32c02ee36a3306397
Wallace Martins <wallacemartinss@gmail.com>
2026-06-26 15:29:55 -0300

    chore: update Docker build args to include redis extension and add health check route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,9 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
+        health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
